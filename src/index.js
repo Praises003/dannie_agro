c247a3b0026289102adbd11e78749d63a480877c
Praises003 <praiseabiodun15@gmail.com>
2025-10-02 13:52:53 -0700

    installed cors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 
 const app = express();
 const { sequelize } = require('./models');
 
 const authRoutes = require('./routes/authRoute');
+const corsOptions = {
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    credentials: true,    
+  };
 
 app.use(express.json()); // parse JSON bodies
 app.use(cookieParser());
+app.use(cors(corsOptions))
 
 // Register auth routes under /api/auth
 app.use('/api/auth', authRoutes);
