8b8bd26e259c696fc325ccacb211f9e02dfd1058
Praises003 <praiseabiodun15@gmail.com>
2026-02-07 17:57:33 +0100

    refactored the httpOnly cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,18 @@
 const authService  = require('../services/authService');
 const isProd = process.env.NODE_ENV === 'production';
-
+const cookieOptions = {
+  httpOnly: true,
+  secure: true,      // REQUIRED for SameSite=None
+  sameSite: "none",  // REQUIRED for cross-origin
+  maxAge: 24 * 60 * 60 * 1000,
+};
 const register = async (req, res) => {
   try {
     const { user, token } = await authService.register(req.body);
 
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 24 * 60 * 60 * 1000, // 1 day
-    });
+    res.cookie('token', token, cookieOptions);
+      
+
 
     res.status(201).json({ message: 'User registered successfully', user });
   } catch (err) {
@@ -24,14 +26,9 @@ const login = async (req, res) => {
   try {
     const { token, user } = await authService.login(req.body);
 
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'None',
-      maxAge: 24 * 60 * 60 * 1000, // 1 day
-    });
+    res.cookie('token', token, cookieOptions);
     
-    res.status(200).json({ message: 'Login successful', token, user });
+    res.status(200).json({ message: 'Login successful',  user });
   } catch (err) {
     res.status(401).json({ error: err.message });
   }
