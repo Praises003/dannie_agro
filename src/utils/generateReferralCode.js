11edcebd973aab48ef7bb18bf9728696bdfb3672
Praises003 <praiseabiodun15@gmail.com>
2026-02-14 17:12:58 +0100

    --just another major updates for all

--- src/utils/generateReferralCode.js
+++ src/utils/generateReferralCode.js
@@ -1,4 +1,4 @@
-export const generateReferralCode = (name) => {
+export default generateReferralCode = (name) => {
   const random = Math.floor(1000 + Math.random() * 9000);
   return name.substring(0, 3).toUpperCase() + random;
 };
