69aad23fc0702b412ef61dcc88dd9cbaa3b96187
Praises003 <praiseabiodun15@gmail.com>
2026-03-14 18:11:29 +0100

    added migrations

--- src/models/index.js
+++ src/models/index.js
@@ -3,37 +3,106 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+
 const basename = path.basename(__filename);
 const config = require(__dirname + '/../config/config.js').development;
 
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+/*
+|--------------------------------------------------------------------------
+| Load all models
+|--------------------------------------------------------------------------
+*/
 
 fs.readdirSync(__dirname)
-  .filter(file => file !== basename && file.endsWith('.js'))
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js'
+    );
+  })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
+
+/*
+|--------------------------------------------------------------------------
+| Model Associations
+|--------------------------------------------------------------------------
+*/
+
+const { User, Product, Cart, CartItem } = db;
+
+/*
+User -> Cart
+*/
+
+if (User && Cart) {
+
+  User.hasOne(Cart, {
+    foreignKey: "userId",
+    onDelete: "CASCADE"
+  });
+
+  Cart.belongsTo(User, {
+    foreignKey: "userId"
+  });
+
+}
+
+/*
+Cart -> CartItem
+*/
+
+if (Cart && CartItem) {
+
+  Cart.hasMany(CartItem, {
+    foreignKey: "cartId",
+    onDelete: "CASCADE"
+  });
+
+  CartItem.belongsTo(Cart, {
+    foreignKey: "cartId"
+  });
+
+}
+
+/*
+Product -> CartItem
+*/
+
+if (Product && CartItem) {
+
+  Product.hasMany(CartItem, {
+    foreignKey: "productId"
+  });
+
+  CartItem.belongsTo(Product, {
+    foreignKey: "productId"
+  });
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| Export DB
+|--------------------------------------------------------------------------
+*/
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-
-db.Cart.belongsTo(db.User, {
-  foreignKey: "userId"
-});
-
-db.Cart.hasMany(db.CartItem, {
-  foreignKey: "cartId"
-});
-
-db.CartItem.belongsTo(db.Cart, {
-  foreignKey: "cartId"
-});
-
-db.CartItem.belongsTo(db.Product, {
-  foreignKey: "productId"
-});
 module.exports = db;
